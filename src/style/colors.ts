1470d55fd93ef492c719699e03bd2abb672c11d8
Quillee <peraltamdb@gmail.com>
2022-07-10 13:00:35 -0400

    Added reference for colors

---
+++ src/style/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  spanishGrey: '#969696',
+  amaryllisRed: '#F5939F',
+  underseasGreen: '#7A8A7F',
+  languidBlue: '#A5B3BF',
+  antiflashWhite: '#F2F2F2',
+  platinum: '#E4E4E4',
+};
+
+export default colors;
